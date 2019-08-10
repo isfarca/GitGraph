d25d3a88951cf055c242ee8e1de7dddabfbbe1e7
Fethi Isfarca <fisfarca@googlemail.com>
2019-08-10 15:17:47 +0200

    Background color
    Dark theme added.

--- gitgraph/src/App.js
+++ gitgraph/src/App.js
@@ -5,7 +5,7 @@ const options =
 {
     template: templateExtend(TemplateName.Metro, {
         colors: ["red", "blue", "orange"],
-        commit: { message: { displayAuthor: false, displayHash: false } }
+        commit: { message: { displayHash: false, displayAuthor: false } }
     }),
     orientation: "vertical-reverse"
 };
@@ -14,29 +14,29 @@ function CreateGraph()
 {
   return (
       <Gitgraph options={options}>
-        {(gitgraph) =>
-        {
-            const master = gitgraph.branch("master");
-            master.commit("Init the project");
-            master
-                .commit("Add README")
-                .commit("Add tests")
-                .commit("Implement feature");
-            master.tag("v1.0");
+          {(gitgraph) =>
+          {
+              const master = gitgraph.branch("master");
+              master.commit("Init the project");
+              master
+                  .commit("Add README")
+                  .commit("Add tests")
+                  .commit("Implement feature");
+              master.tag("v1.0");
 
-            const newFeature = gitgraph.branch("new-feature");
-            newFeature.commit("Implement an awesome feature");
+              const newFeature = gitgraph.branch("new-feature");
+              newFeature.commit("Implement an awesome feature");
 
-            const bla = gitgraph.branch("bla");
-            bla.commit("Implement an awesome bla");
-            newFeature.merge(bla);
+              const bla = gitgraph.branch("bla");
+              bla.commit("Implement an awesome bla");
+              newFeature.merge(bla);
 
-            master.commit("Hotfix a bug");
-            newFeature.commit("Fix tests");
+              master.commit("Hotfix a bug");
+              newFeature.commit("Fix tests");
 
-            // Merge `newFeature` into `master`
-            master.merge(newFeature, "Release new version");
-        }}
+              // Merge `newFeature` into `master`
+              master.merge(newFeature, "Release new version");
+          }}
       </Gitgraph>
   );
 }
