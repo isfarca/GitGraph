b396e2eb3457570cd923c798dea87461a3335169
Fethi Isfarca <fisfarca@googlemail.com>
2019-08-10 22:40:15 +0200

    Default git flow graph
    Git flow graph base defined.

--- gitgraph/src/components/graph.js
+++ gitgraph/src/components/graph.js
@@ -20,30 +20,72 @@ class Graph extends Component
     // Create the git flow graph.
     static CreateGraph(gitgraph)
     {
-        const master = gitgraph.branch("master");
-        master.commit("Init the project");
-        master
-            .commit("Add README")
-            .commit("Add tests")
-            .commit("Implement feature");
-        master.tag("v1.0");
+        const flow = gitgraph.branch({name: "flow", from: null});
+        const master = gitgraph.branch({name: "master", from: null});
+        flow.commit("Initialize flow commit.");
+        flow.commit("Define git flow.");
+        master.commit("Initialize master commit.");
 
-        const newFeature = gitgraph.branch("new-feature");
-        newFeature.commit("Implement an awesome feature");
+        const develop = gitgraph.branch({name: "develop", from: master});
+        develop.commit("Initialize develop commit.");
 
-        const bla = gitgraph.branch("bla");
-        bla.commit("Implement an awesome bla");
-        newFeature.merge(bla);
+        const feature = gitgraph.branch({name: "feature", from: develop});
+        feature.commit("Initialize feature commit.");
+        const feature1 = gitgraph.branch({name: "feature1", from: feature});
+        const feature2 = gitgraph.branch({name: "feature2", from: feature});
+        const feature3 = gitgraph.branch({name: "feature3", from: feature});
+        const hotfix = gitgraph.branch({name: "hotfix", from: develop});
+        hotfix.commit("Initialize hotfix commit.");
+        const hotfix1 = gitgraph.branch({name: "hotfix1", from: hotfix});
+        const hotfix2 = gitgraph.branch({name: "hotfix2", from: hotfix});
+        const hotfix3 = gitgraph.branch({name: "hotfix3", from: hotfix});
 
-        const xd = gitgraph.branch("xd");
-        xd.commit("Implement an awesome xd");
-        bla.merge(xd);
+        feature1.commit("Initialize feature1 commit.");
+        feature1
+            .commit("feature1 commit 1.")
+            .commit("feature1 commit 2.")
+            .commit("feature1 commit 3.");
+        feature2.commit("Initialize feature2 commit.");
+        feature2
+            .commit("feature2 commit 1.")
+            .commit("feature2 commit 2.")
+            .commit("feature2 commit 3.");
+        feature3.commit("Initialize feature3 commit.");
+        feature3
+            .commit("feature3 commit 1.")
+            .commit("feature3 commit 2.")
+            .commit("feature3 commit 3.");
+        feature.merge(feature1);
+        feature.merge(feature2);
+        feature.merge(feature3);
 
-        master.commit("Hotfix a bug");
-        newFeature.commit("Fix tests");
+        develop.merge(feature);
 
-        // Merge `newFeature` into `master`
-        master.merge(newFeature, "Release new version");
+        hotfix1.commit("Initialize hotfix1 commit.");
+        hotfix1
+            .commit("hotfix1 commit 1.")
+            .commit("hotfix1 commit 2.")
+            .commit("hotfix1 commit 3.");
+        hotfix2.commit("Initialize hotfix2 commit.");
+        hotfix2
+            .commit("hotfix2 commit 1.")
+            .commit("hotfix2 commit 2.")
+            .commit("hotfix2 commit 3.");
+        hotfix3.commit("Initialize hotfix3 commit.");
+        hotfix3
+            .commit("hotfix3 commit 1.")
+            .commit("hotfix3 commit 2.")
+            .commit("hotfix3 commit 3.");
+        hotfix.merge(hotfix1);
+        hotfix.merge(hotfix2);
+        hotfix.merge(hotfix3);
+
+        develop.merge(hotfix);
+
+        master.merge(develop);
+        master.tag("V 1.0");
+
+        flow.commit("Expand git flow.");
     }
 }
 
