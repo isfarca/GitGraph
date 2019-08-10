f541c56e8c2eed7795472eb4b1eeab507e5a7d3c
Fethi Isfarca <fisfarca@googlemail.com>
2019-08-10 13:07:45 +0200

    MyComponent to CreateGraph
    Function name 'MyComponent' changed to 'CreateGraph'.

--- gitgraph/src/App.js
+++ gitgraph/src/App.js
@@ -1,30 +1,32 @@
 import React from 'react';
 const { Gitgraph } = require("@gitgraph/react");
 
-function MyComponent() {
+function CreateGraph()
+{
   return (
       <Gitgraph>
-        {(gitgraph) => {
-          // Simulate git commands with Gitgraph API.
-          const master = gitgraph.branch("master");
-          master.commit("Initial commit");
+        {(gitgraph) =>
+        {
+            // Simulate git commands with Gitgraph API.
+            const master = gitgraph.branch("master");
+            master.commit("Initial commit");
 
-          const develop = gitgraph.branch("develop");
-          develop.commit("Add TypeScript");
+            const develop = gitgraph.branch("develop");
+            develop.commit("Add TypeScript");
 
-          const aFeature = gitgraph.branch("a-feature");
-          aFeature
-              .commit("Make it work")
-              .commit("Make it right")
-              .commit("Make it fast");
+            const aFeature = gitgraph.branch("a-feature");
+            aFeature
+                .commit("Make it work")
+                .commit("Make it right")
+                .commit("Make it fast");
 
-          develop.merge(aFeature);
-          develop.commit("Prepare v1");
+            develop.merge(aFeature);
+            develop.commit("Prepare v1");
 
-          master.merge(develop).tag("v1.0.0");
+            master.merge(develop).tag("V 1.0");
         }}
       </Gitgraph>
   );
 }
 
-export default MyComponent;
+export default CreateGraph;
